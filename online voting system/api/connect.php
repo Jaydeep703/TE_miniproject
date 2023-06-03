febc3caf57972580fb1409a593659eef566c5229
Jaydeep703 <95301110+Jaydeep703@users.noreply.github.com>
2023-06-03 10:48:16 +0530

    Add files via upload

---
+++ online voting system/api/connect.php
@@ -0,0 +1,11 @@
+<?php
+
+$connect = mysqli_connect("localhost","root","Jaydeep@703","voting") or die("Connection Failed");
+// if($connect){
+//     echo "Database connected";
+// } 
+// else{
+//     echo "Database not connected !";
+// }
+
+?>
